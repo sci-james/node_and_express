9056009b69b1db39cba41296ce57cb167c7d2429
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2020-10-11 01:53:51 +0530

    Node.js

--- 6_create_backend/index.js
+++ 6_create_backend/index.js
@@ -7,17 +7,34 @@ const port = 3000;
 const home = fs.readFileSync('index.htm');
 const about = fs.readFileSync('about.htm');
 const contact = fs.readFileSync('contact.htm');
+const services = fs.readFileSync('services.htm');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  url = req.url;
 
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url)
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(home);
-      });
-})
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
 
+  if(url == '/'){
+
+    res.end(home);
+  }
+  else if(url == '/about'){
+    res.end(about);
+  }
+  else if(url == '/services'){
+    res.end(srvices)
+  }
+  else if(url == '/contact'){
+    res.end(contact)
+  }
+  else{
+    res.statusCode = 404;
+    res.end("<h1>404 not found</h1>");
+  }
+});
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # start backend with node-js
-We are starting backend with node js, express js and mongoDB database
+### We are starting backend with node js, express js and mongoDB database
 
 ## <a href="1_intro_node">Introduction to Node js, What is REPL? </a>
-## <a href="2_start">Start doing some work with node.js <br>
-Serve a html page </a>
+## <a href="2_start">Start doing some work with node.js</a>
 ## <a href="3_node_modules">Node js Modules </a>
+## <a href="4_block_non_block_code">Synchronous and Asynchronous </a>
+## <a href="5_serving_html"> Serving HTML files</a>
+## <a href="6_create_backend">Create a custom backend </a>
+## <a href="7_create_modules">Creating custom modules </a>
+
