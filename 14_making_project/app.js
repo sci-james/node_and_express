30c787886fe16f97e4ad0456c4b166bcfe8e53bc
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2020-10-27 19:05:30 +0530

    Using react-express-views

---
+++ 14_making_project/app.js
@@ -0,0 +1,29 @@
+// include modules
+const express = require("express");
+const path = require("path");
+// const fs = require('fs');
+const app = express();
+const port = 80;
+
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')); // For serving static files
+app.use(express.urlencoded());
+
+
+//PUG SPECIFIC STUFF
+app.set('view engine','pug'); // set the view engine as pug
+app.set('views', path.join(__dirname,'views')); // set the views directory
+
+
+// ENDPOINTS
+app.get('/', (req,res)=>{
+    // const params = {};
+    res.status(200).render('index.pug');
+});
+
+
+//START THE SERVER
+app.listen(port,()=>{
+    console.log(`The application is running on the port ${port}`);
+});
