9b83e914b46a695f496ebe6fc4932b9f10cb211b
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2020-11-22 21:28:43 +0530

    Complete project website with node js, saving data to the mongodb database using mongoose

--- 14_making_project/app.js
+++ 14_making_project/app.js
@@ -2,8 +2,24 @@
 const express = require("express");
 const path = require("path");
 // const fs = require('fs');
+const bodyparser = require("body-parser");
 const app = express();
 const port = 80;
+
+// _______MONGOOSE______
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dancecontactform', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: Number,
+    email: String,
+    address: String,
+    more: String
+});
+
+const Contact = mongoose.model('contact',contactSchema);
 
 
 // EXPRESS SPECIFIC STUFF
@@ -26,7 +42,14 @@ app.get('/contact', (req,res)=>{
     // const params = {};
     res.status(200).render('contact.pug');
 });
-
+app.post('/contact', (req,res)=>{
+    const mydata = new Contact(req.body);
+    mydata.save().then(()=>{
+        res.send("your message has been sent");
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database");
+    });
+});
 
 //START THE SERVER
 app.listen(port,()=>{
